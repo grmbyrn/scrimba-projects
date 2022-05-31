5694663d8855f1263b3dcb92892eef2cc20acf4f
grmbyrn <graemebyrne_89@hotmail.com>
2022-05-31 23:42:33 +0100

    added buttons to connect to cards api and styled css

--- war-project/index.js
+++ war-project/index.js
@@ -10,3 +10,15 @@ function handleClick(){
 }
 
 document.getElementById('new-deck').addEventListener('click', handleClick)
+document.getElementById('draw-cards').addEventListener('click', drawCards)
+
+function drawCards(){
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById('cards').innerHTML = `
+            <img src='${data.cards[0].image}' class='card'>
+            <img src='${data.cards[1].image}' class='card'>
+        `
+    })
+}
