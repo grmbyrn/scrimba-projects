c51568355bf54708da7cf54521c627bf92966db9
grmbyrn <graemebyrne_89@hotmail.com>
2022-06-03 00:26:05 +0100

    added functionality to count scores and find and display winner and also converted appropriate functions to async await

--- war-project/index.js
+++ war-project/index.js
@@ -1,41 +1,48 @@
 let deckId
+let computerScore = 0
+let playerScore = 0
 const cardsContainer = document.getElementById('cards')
 const newDeckBtn = document.getElementById('new-deck')
 const drawCardsBtn = document.getElementById('draw-cards')
 const header = document.getElementById('header')
 const remainingCards = document.getElementById('remaining')
+const computerScoreEl = document.getElementById('computer-score')
+const playerScoreEl = document.getElementById('player-score')
 
-function handleClick(){
-    fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-    .then(res => res.json())
-    .then(data => {
-        remainingCards.innerHTML = `Remaining Cards: ${data.remaining}`
-        deckId = data.deck_id
-        console.log(deckId)
-    })
+async function handleClick(){
+    const res = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+    const data = await res.json()
+    remainingCards.innerHTML = `Remaining Cards: ${data.remaining}`
+    deckId = data.deck_id
+    console.log(deckId)
 }
 
 newDeckBtn.addEventListener('click', handleClick)
 drawCardsBtn.addEventListener('click', drawCards)
 
-function drawCards(){
-    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
-    .then(res => res.json())
-    .then(data => {
-        remainingCards.innerHTML = `Remaining Cards: ${data.remaining}`
-        cardsContainer.children[0].innerHTML = `
-            <img src='${data.cards[0].image}' class='card'>
-        `
-        cardsContainer.children[1].innerHTML = `
-            <img src='${data.cards[1].image}' class='card'>
-        `
-        const winnerText = determineWhoWins(data.cards[0], data.cards[1])
-        header.textContent = winnerText
+async function drawCards(){
+    const res = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+    const data = await res.json()
+    remainingCards.innerHTML = `Remaining Cards: ${data.remaining}`
+    cardsContainer.children[0].innerHTML = `
+        <img src='${data.cards[0].image}' class='card'>
+    `
+    cardsContainer.children[1].innerHTML = `
+        <img src='${data.cards[1].image}' class='card'>
+    `
+    const winnerText = determineWhoWins(data.cards[0], data.cards[1])
+    header.textContent = winnerText
 
-        if(data.remaining === 0){
-            drawCardsBtn.disabled = true
+    if(data.remaining === 0){
+        drawCardsBtn.disabled = true
+        if(computerScore > playerScore){
+            header.textContent = 'The computer wins!'
+        } else if (computerScore < playerScore){
+            header.textContent = 'The player wins!'
+        } else if(computerScore === playerScore){
+            header.textContent = 'War!'
         }
-    })
+    }
 }
 
 function determineWhoWins(card1, card2){
@@ -44,20 +51,14 @@ function determineWhoWins(card1, card2){
     const cardOptionValue2 = cardValueOptions.indexOf(card2.value)
 
     if(cardOptionValue1 > cardOptionValue2){
-        return 'Player 1 Wins'
+        computerScore++
+        computerScoreEl.innerHTML = `Computer Score: ${computerScore}`
+        return 'Computer Wins'
     } else if(cardOptionValue1 < cardOptionValue2){
-        return 'Player 2 Wins'
+        playerScore++
+        playerScoreEl.innerHTML = `Player Score: ${playerScore}`
+        return 'Player Wins'
     } else {
-        return 'Tie'
+        return 'War'
     }
 }
-
-// const cardObj1 = {
-//     value: 'JACK'
-// }
-
-// const cardObj2 = {
-//     value: '2'
-// }
-
-// determineWhoWins(cardObj1, cardObj2)
