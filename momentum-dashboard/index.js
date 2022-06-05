0ff12956a02a301b2d13d8700f48e3146608a033
grmbyrn <graemebyrne_89@hotmail.com>
2022-06-06 00:27:36 +0100

    added unsplash api and began styling with css

--- momentum-dashboard/index.js
+++ momentum-dashboard/index.js
@@ -1,6 +1,10 @@
-fetch('https://api.unsplash.com/photos/random?orientation=landscape&query=nature')
+let clientID = 'Zit5m2i8VP7r_ymbtJqzuo3pMNXH9UemBcMS7TwLq8U'
+let endpoint = `https://api.unsplash.com/photos/random/?client_id=${clientID}`
+
+fetch(endpoint)
     .then(res => res.json())
     .then(data => {
         console.log(data)
         document.body.style.backgroundImage = `url(${data.urls.full})`
+        document.getElementById('author').textContent = `By: ${data.user.last_name} ${data.user.first_name}`
     })
