ba1c9a2c380d1c0b5fa88978034343a4e67030d4
grmbyrn <graemebyrne_89@hotmail.com>
2022-06-07 23:45:54 +0100

    added crypto info from api

--- momentum-dashboard/index.js
+++ momentum-dashboard/index.js
@@ -8,3 +8,19 @@ fetch(endpoint)
         document.body.style.backgroundImage = `url(${data.urls.full})`
         document.getElementById('author').textContent = `By: ${data.user.last_name} ${data.user.first_name}`
     })
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Something went wrong")
+        }
+        return res.json()
+    })
+    .then(data => {
+        console.log(data)
+        document.querySelector('#doge').innerHTML = `
+            <img src='${data.image.small}'>
+            <p>${data.name}</p>
+        `
+    })
+    .catch(err => console.error(err))
